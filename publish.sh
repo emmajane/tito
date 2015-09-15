a21519977bd6ac28325223aa8976083ebb9da008
emmajane <emma@emmajane.net>
2015-09-15 12:46:10 +0100

    Configure the publish script for League of Awesome hosting.

--- publish.sh
+++ publish.sh
@@ -7,5 +7,6 @@
 sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
-rsync -avze 'ssh -p 4668' output_prod/ username@yoursculpinsite:public_html
+# rsync -avze 'ssh -p 4668' output_prod/ emmajane@leagueofawesome.com:/var/www/static/trainingintheopen.com
+rsync -avz --delete output_prod/* emmajane@leagueofawesome.com:/var/www/static/trainingintheopen.com/.
 if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
